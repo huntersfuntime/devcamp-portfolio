2605392a50ea789d07bd346e32580f80ed67dacf
huntersfuntime <hunterorges@gmail.com>
2017-02-24 12:09:13 -0700

    Integrated new

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,5 +3,24 @@ class PortfoliosController < ApplicationController
 		@portfolio_items = Portfolio.all
 	end
 
+def new
+	@portfolio_item = Portfolio.new
+end
+
+def create
+	@portfolio_item = Portfolio.new(params.require(:protfolio).permit(:title, :subtitle, :body))
+ respond_to do |format|
+ 	
+ 	if @blog.save
+ 		format.html { redirect_to portfolio_path, notice: 'Your portolio item is is now live.'}
+ 	
+ 	else
+ 		
+ 		
+ 	
+ 	end
+ end
+end
+
 
 end
