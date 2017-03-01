b7e856d9fa4ad2276f1fa9f9047ef4075f72ccc0
huntersfuntime <hunterorges@gmail.com>
2017-02-28 21:35:49 -0700

    implemented custom scopes for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
+3.times do |topic|
+	Topic.create!(
+		title: "Topic #{topic}"
+		)
+end
+
 
 10.times do |blog|
 	Blog.create!(
@@ -7,7 +13,8 @@
 		Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et
 		Malorum by Cicero are also reproduced in their exact 
 		original form, accompanied by English versions from the 
-		1914 translation by H. Rackham."
+		1914 translation by H. Rackham.",
+		topic_id: Topic.last.id
 		)
 end
 
@@ -22,15 +29,27 @@ end
 
 puts "5 skills created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
 	Portfolio.create!(
 	title: "Portfolio title: #{portfolio_item}",
-	subtitle: "My great service",
+	subtitle: "Ruby on rails",
 	body: "Who what what what what
 	what what what what what what what",
 	main_image:  "http://placehold.it/600x400" , 
 	thumb_image: "http://placehold.it/350x200"
 	)
 end
+
+1.times do |portfolio_item|
+	Portfolio.create!(
+	title: "Portfolio title: #{portfolio_item}",
+	subtitle: "Angular",
+	body: "Who what what what what
+	what what what what what what what",
+	main_image:  "http://placehold.it/600x400" , 
+	thumb_image: "http://placehold.it/350x200"
+	)
+end
+
 
 puts "9 portfolios created"
