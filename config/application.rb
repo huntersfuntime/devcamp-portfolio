a0ab958a96ac14a2f239ac04446c98ca30dc7e96
huntersfuntime <hunterorges@gmail.com>
2017-03-02 14:02:04 -0700

    Refactored strong params for portfolio items

--- config/application.rb
+++ config/application.rb
@@ -8,15 +8,15 @@ require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
-require "action_cable/engine"
 require "sprockets/railtie"
+require "action_cable/engine"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
 module DevcampPortfolio
+
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
