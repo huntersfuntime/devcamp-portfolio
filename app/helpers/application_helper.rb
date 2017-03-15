b83f86522fff0e63b193b199edb3309a0878c10a
huntersfuntime <hunterorges@gmail.com>
2017-03-15 13:47:23 -0600

    Refactored alerts into help method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -60,3 +60,31 @@ module ApplicationHelper
   end
 
 end
+
+def alerts
+  alert = (flash[:alert] || flash[:error] || flash[:notice])
+  if alert
+    alert_generator alert
+  end
+end
+
+def alert_generator msg
+  js add_gritter(msg, title: "Hunter Orges Portfolio", sticky: false) 
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
