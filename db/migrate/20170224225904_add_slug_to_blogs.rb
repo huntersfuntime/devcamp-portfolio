84face3680591cdfad3f441b4fdbcc2f0ad504df
huntersfuntime <hunterorges@gmail.com>
2017-03-21 10:04:33 -0600

    changed migration file

--- db/migrate/20170224225904_add_slug_to_blogs.rb
+++ db/migrate/20170224225904_add_slug_to_blogs.rb
@@ -1,5 +1,6 @@
-class AddPostStatusToBlogs < ActiveRecord::Migration[5.0]
+class AddSlugToBlogs < ActiveRecord::Migration[5.0]
   def change
-    add_column :blogs, :status, :integer, default: 0
+    add_column :blogs, :slug, :string
+    add_index :blogs, :slug, unique: true
   end
 end
